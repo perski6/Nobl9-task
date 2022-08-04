00ac41b8d9a81a307dac364ca41e7930cc3a39d8
perski6 <igor.grzankowski@gmail.com>
2022-08-04 16:11:53 +0200

    add loading api key from .env

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module perski6.com/nobl9-task
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
+)
--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"perski6.com/nobl9-task/constants"
 	"perski6.com/nobl9-task/handlers"
 )
 
 func main() {
+	constants.LoadEnv()
 	r := mux.NewRouter()
 	r.HandleFunc("/random/mean", handlers.RandomsHandler).Methods(http.MethodGet).Queries("requests", "{requests:[0-9]+}", "length", "{length:[0-9]+}")
 	log.Fatal(http.ListenAndServe(":8080", r))
