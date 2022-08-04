1746e6f409fa7257ffeef6ed2ecd93c606e91e42
perski6 <igor.grzankowski@gmail.com>
2022-08-04 12:52:11 +0200

    add endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module perski6.com/nobl9-task
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"perski6.com/nobl9-task/handlers"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	r := mux.NewRouter()
+	r.HandleFunc("/random/mean", handlers.RandomsHandler).Methods(http.MethodGet).Queries("requests", "{requests:[0-9]+}", "length", "{length:[0-9]+}")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
